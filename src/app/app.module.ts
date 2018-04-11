c3f2835ad0993109f79201d5f633f501fd1c1730
mintaras <balnys.mintaras@gmail.com>
2018-04-11 10:24:42 +0300

    created heroes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HereosComponent } from './hereos/hereos.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HereosComponent
   ],
   imports: [
     BrowserModule,
