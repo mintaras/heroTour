51be51e10b782ea6e7b10ef4e64181c85016d491
mintaras <balnys.mintaras@gmail.com>
2018-04-12 11:08:04 +0300

    Adds HeroService, MessagesService, MessagesComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,26 @@ import { AppComponent } from './app.component';
 import { HereosComponent } from './hereos/hereos.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
+import { HeroService } from './hero.service';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HereosComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    HeroService,
+    MessageService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
